636351596e324067687ad1e7e6e7ddbf3f2bbc25
Oli <oli@appyvet.com>
2021-10-09 10:06:06 +0100

    Use interface not extension for 0n1 contract typing

--- contracts/0n1Gear.sol
+++ contracts/0n1Gear.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/interfaces/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
